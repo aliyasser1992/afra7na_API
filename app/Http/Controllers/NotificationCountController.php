42d50687e19244b80deef13ef9988e0b41755090
mkchemist <m.k_chemist@yahoo.com>
2022-08-22 22:21:04 +0200

    reimplement of notification counter

--- app/Http/Controllers/NotificationCountController.php
+++ app/Http/Controllers/NotificationCountController.php
@@ -10,24 +10,44 @@ class NotificationCountController extends Controller
     //
     public function NotificationCount()
     {
-        $user = new User();
-        if (isset(request()->user()->id)) {
-            $userData = $user->where('id', request()->user()->id)->first();
-            $last_seen_notification = $userData->notification != '' ? $userData->notification : '';
-            $count_notification = adsNotifications::where('country_id', auth()->user()->country_id);
-            if (auth()->user()->region_id) {
-                $count_notification = $count_notification->where('region_id', auth()->user()->region_id);
-            }
-            if ($last_seen_notification != '') :
-                $count_notification = $count_notification->where('created_at', '>', $last_seen_notification);
-            endif;
-            $count_notification = $count_notification->count();
-            $update_last_seen = $user->where('id', request()->user()->id)->update([
-                'notification' => date("Y-m-d H:i:s"),
+        $user = request()->user();
+
+        if (!$user) {
+            return response()->json([
+                'state' => 'Success',
+                'count' => 0,
+                'message' => 'No Authenticated user'
             ]);
-            return ['state' => 'success', 'count' => $count_notification];
-        } else {
-            return ['state' => 'success', 'count' => 0];
         }
+
+        $counter = adsNotifications::where('region_id', $user->region_id)
+        ->count("*");
+
+        return response()->json([
+            'state' => 'Success',
+            'count' => $counter,
+            'user' => $user->name
+        ]);
+
+
+        // $user = new User();
+        // if (isset(request()->user()->id)) {
+        //     $userData = $user->where('id', request()->user()->id)->first();
+        //     $last_seen_notification = $userData->notification != '' ? $userData->notification : '';
+        //     $count_notification = adsNotifications::where('country_id', auth()->user()->country_id);
+        //     if (auth()->user()->region_id) {
+        //         $count_notification = $count_notification->where('region_id', auth()->user()->region_id);
+        //     }
+        //     if ($last_seen_notification != '') :
+        //         $count_notification = $count_notification->where('created_at', '>', $last_seen_notification);
+        //     endif;
+        //     $count_notification = $count_notification->count();
+        //     $update_last_seen = $user->where('id', request()->user()->id)->update([
+        //         'notification' => date("Y-m-d H:i:s"),
+        //     ]);
+        //     return ['state' => 'success', 'count' => $count_notification];
+        // } else {
+        //     return ['state' => 'success', 'count' => 0];
+        // }
     }
 }
